6fcfffc49891a49f1e1b52f7ab72182a45b8990e
César Benito Lamata <cesarbema2009@hotmail.com>
2016-09-14 09:10:34 +0200

    Added script for Renfe 440 circuit breaker

---
+++ Script/Disyuntor_440.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using ORTS.Common;
+using ORTS.Scripting.Api;
+using Orts.Simulation;
+
+namespace ORTS.Scripting.Script
+{
+    public class Disyuntor_269 : CircuitBreaker
+    {
+        private Timer ClosingTimer;
+
+        public override void Initialize()
+        {
+            SetDriverClosingAuthorization(true);
+            ClosingTimer = new Timer(this);
+            ClosingTimer.Setup(ClosingDelayS());
+        }
+        PantographState PantoPrevState;
+        public override void Update(float elapsedSeconds)
+        {
+            SetClosingAuthorization(TCSClosingAuthorization() && DriverClosingAuthorization());
+            switch(CurrentState())
+            {
+                case CircuitBreakerState.Open:
+                    if(ClosingAuthorization() && DriverClosingOrder())
+                    {
+                        SetCurrentState(CircuitBreakerState.Closing);
+                    }
+                    break;
+                case CircuitBreakerState.Closing:
+                    if(ClosingAuthorization() && DriverClosingOrder())
+                    {
+                        if (!ClosingTimer.Started) ClosingTimer.Start();
+                        if (ClosingTimer.Triggered) SetCurrentState(CircuitBreakerState.Closed);
+                    }
+                    else
+                    {
+                        ClosingTimer.Stop();
+                        SetCurrentState(CircuitBreakerState.Open);
+                    }
+                    break;
+                case CircuitBreakerState.Closed:
+                    if (!ClosingAuthorization()) SetCurrentState(CircuitBreakerState.Open);
+                    break;
+            }
+        }
+        public override void HandleEvent(PowerSupplyEvent evt)
+        {
+            switch (evt)
+            {
+                case PowerSupplyEvent.CloseCircuitBreakerButtonPressed:
+                    SetDriverClosingOrder(true);
+                    break;
+                case PowerSupplyEvent.CloseCircuitBreakerButtonReleased:
+                    SetDriverClosingOrder(false);
+                    break;
+            }
+        }
+    }
+}
